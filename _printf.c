93486b906f1d75fc43208cf30ec837273d7fbf1f
Nina Mwangi <122353603+NinaMwangi@users.noreply.github.com>
2023-03-28 10:11:18 +0300

    Task 0

---
+++ _printf.c
@@ -0,0 +1,58 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+
+/**
+ * _printf - produces output according to a format
+ * @format: the string with the format specifiers
+ * Return: numb of chars printed to std output*
+ */
+
+int _printf(const char *format, ...)
+{
+        va_list list;
+        int count = 0;
+        char *pH;
+
+        va_start(list, format);
+
+        while (*format != '\0')
+        {
+                if (*format == '%')
+                {
+                        if (*(format + 1) == 'c')
+                        {
+                                _putchar(va_arg(list, int));
+                                count++;
+                                format += 2;
+                        }
+                        else
+                        if (*(format + 1) == 's')
+                        {
+                                pH = va_arg(list, char *);
+
+                                while (*pH != '\0')
+                                {
+                                        _putchar(*pH);
+                                        pH++;
+                                        count++;
+                                }
+                                format += 2;
+                        }
+                        else if (*(format + 1) == '%')
+                        {
+                                _putchar('%');
+                                format += 2;
+                                count++;
+                        }
+                }
+
+                _putchar(*format);
+                format++;
+                count++;
+        }
+
+        va_end(list);
+        return (count);
+}
