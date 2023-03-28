a5bfa0cd3b78a1796f0c423e31ec9ae1edf4653a
Nina Mwangi <122353603+NinaMwangi@users.noreply.github.com>
2023-03-28 12:03:29 +0300

    Task 0

--- _printf.c
+++ _printf.c
@@ -15,8 +15,6 @@ int _printf(const char *format, ...)
 	int count = 0;
 	char *pH;
 
-	if (format =! NULL)
-	{
 	va_start(list, format);
 
 	while (*format != '\0')
@@ -56,6 +54,5 @@ int _printf(const char *format, ...)
 	}
 
 	va_end(list);
-	}
 	return (count);
 }
